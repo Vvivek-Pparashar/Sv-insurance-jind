cba27e3f68e6a10d19111e723aac529a688fcdb4
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2024-06-03 07:09:20 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import React, { Suspense } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import HeroPage from "./components/HomePage/HeroPage";
 import About from "./components/HomePage/About";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import { ScrollRestoration } from "react-router-dom";
 import BottomTele from "./components/BottomTele";
-import Testimonial from "./components/HomePage/Testimonial";
-import TeamMember from "./components/HomePage/TeamMember";
-import Services from "./components/HomePage/Services";
-import Features from "./components/HomePage/Features";
+// import Testimonial from "./components/HomePage/Testimonial";
+// import TeamMember from "./components/HomePage/TeamMember";
+// import Services from "./components/HomePage/Services";
+// import Features from "./components/HomePage/Features";
 // import FactMap from "./components/HomePage/FactMap";
+const Features = React.lazy(() => import("./components/HomePage/Features"));
+const Services = React.lazy(() => import("./components/HomePage/Services"));
+const TeamMember = React.lazy(() => import("./components/HomePage/TeamMember"));
+const Testimonial = React.lazy(() =>
+  import("./components/HomePage/Testimonial")
+);
+const Footer = React.lazy(() => import("./components/Footer/Footer"));
 const FactMap = React.lazy(() => import("./components/HomePage/FactMap"));
 import "./App.css";
 
@@ -17,10 +24,10 @@ const App = () => {
   return (
     <div>
       {/* <MyErrorBoundary> */}
-      <Suspense fallback={<p>Loading....</p>}>
-        <NavBar page={"Home"} />
-        <HeroPage />
-        <About />
+      <NavBar page={"Home"} />
+      <HeroPage />
+      <About />
+      <Suspense fallback={<h3>Loading....</h3>}>
         <FactMap />
         <Features />
         <Services />
